4a01beb13e7ca0617b3c57cd24d265b865ae9082
osmanceyhan <oceyhan@miniyol.com>
2024-01-21 17:36:25 +0300

    Auth Controller + Readme.md update

--- resources/js/components/ChatComponent.vue
+++ resources/js/components/ChatComponent.vue
@@ -48,8 +48,11 @@
         </div>
 
         <div class="row mx-2">
-            <div class="my-2 p-0 col-12 text-center ">
-                <button class="btn btn-primary col-12" style="max-width: 90%" @click="subscribeToPublicChannel">Join public room</button>
+            <div class="my-2 p-0 col-6 text-start ">
+                <button class="btn btn-primary" style="width: 380px; max-width: 90%" @click="subscribeToPublicChannel">Join public room</button>
+            </div>
+            <div class="my-2 p-0 col-6 text-end">
+                <button class="btn btn-primary" style="width: 380px; max-width: 90%" @click="subscribeToPrivateAndPresenceChannel">Join private room</button>
             </div>
         </div>
     </div>
@@ -155,7 +158,155 @@ export default {
             });
         },
 
+        subscribeToPrivateAndPresenceChannel(event) {
+            if(!window.authUser)
+                this.$swal({
+                    title: 'You are a guest user, please sign up for joining private channels.',
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes, I want to register',
+                    cancelButtonText: 'Stay on the homepage',
+                    customClass: {
+                        confirmButton: 'btn-alt-confirm btn-confirm-custom-event',
+                        cancelButton: 'btn-secondary',
+                        title: 'custom-title'
+                    }
+                }).then(function (result) {
+                    if (result.value) {
+                        window.location = "/register"
+                    }
+                });
+            else
+                this.$swal({
+                    input: 'text',
+                    title: 'Enter the private room name <br>  (e.g. room-1, room-2)',
+                    showCancelButton: true,
+                    confirmButtonText: 'OK',
+                    cancelButtonText: 'Cancel',
+                    customClass: {
+                        confirmButton: 'btn-alt-confirm',
+                        cancelButton: 'btn-secondary',
+                        title: 'custom-title'
+                    }
+                }).then(r => {
+                    let channelName = r.value
+                    if (!channelName?.trim().length) {
+                        return;
+                    }
+                    channelName = channelName.trim();
 
+                    // Register and subscribe to events on the private channel.
+                    Echo.private(channelName)
+                        .subscribed(() => {
+                            const channel = new Channel({
+                                type: 'private',
+                                name: channelName
+                            });
+
+                            this.tabs.push(channel);
+                            this.setActiveChannelIndex(this.tabs.length - 1);
+                            this.updateStatusMessageOnUI(channel, "Subscribed to private room " + channelName);
+                        })
+                        .listenToAll((eventName, data) => {
+                            console.log("Event ::  " + eventName + ", data is ::" + JSON.stringify(data));
+                        })
+                        .listen('PrivateMessageEvent', (data) => {
+                            const channel = this.getChannelByName(channelName, 'private');
+                            this.updateBroadcastNotificationOnUI(channel, data)
+                        })
+                        .listenForWhisper('message', (data) => {
+                            const channel = this.getChannelByName(channelName, 'private');
+                            this.updateUserMessageOnUI(channel, data.message, data.user);
+                        })
+                        .listenForWhisper('typing', (data) => {
+                            let user = data.user;
+                            if(!user)
+                                return;
+
+                            const channel = this.getChannelByName(channelName, 'private');
+                            clearTimeout(channel.typingStopEvents[user]);
+
+                            if(!channel.typingNames.includes(user))
+                                channel.typingNames.push(user);
+
+                            channel.typingStopEvents[user] = setTimeout(() => {
+                                delete channel.typingStopEvents[user];
+                                channel.typingNames = channel.typingNames.filter(e => e !== user);
+                            }, 1500);
+
+                        })
+                        .error((err) => {
+                            if( err === 403 || err?.statusCode === 403) {
+                                if (!window.authUser) {
+                                    this.$swal({
+                                        title: 'You don\'t have the access to join this private room, try logging into the application.',
+                                        buttonsStyling: false,
+                                        showCancelButton: true,
+                                        confirmButtonClass: 'btn-alt-confirm',
+                                        cancelButtonClass: 'btn-secondary',
+                                        titleClass: 'custom-title'
+                                    });
+                                } else {
+                                    this.$swal({
+                                        title: 'Please enter room name in the format room-1, room-2, room-3 etc. Other rooms are not accessible',
+                                        buttonsStyling: false,
+                                        showCancelButton: true,
+                                        confirmButtonClass: 'btn-alt-confirm',
+                                        cancelButtonClass: 'btn-secondary',
+                                        titleClass: 'custom-title'
+                                    });
+                                }
+                            }
+                            else
+                                alert("An error occurred while trying to join a private room, check the console for details.");
+
+                            console.error(err);
+                        });
+
+                    // Register and subscribe to presence changes on presence channel.
+                    Echo.join(channelName)
+                        .subscribed(()=> {
+                            console.log(channelName, "Subscribed to presence room " + channelName);
+                        })
+                        .here((members) => {
+                            const channel = this.getChannelByName(channelName, 'private');
+
+                            if(members.length <= 1)
+                                this.updateStatusMessageOnUI(channel, "There are no other users in this room");
+                            else
+                                this.updateStatusMessageOnUI(channel, "There are " + members.length + " users in this room");
+
+                            channel.memberCount = members.length;
+                            console.log("List of members: " + JSON.stringify(members));
+                        })
+                        .joining((data) => {
+                            const channel = this.getChannelByName(channelName, 'private');
+
+                            if (data?.name)
+                                this.updateStatusMessageOnUI(channel, data.name + " joined the room");
+                            else
+                                this.updateStatusMessageOnUI(channel, "User " + data + " joined the room");
+
+                            console.log(data, "joined room");
+                        })
+                        .leaving((data) => {
+                            const channel = this.getChannelByName(channelName, 'private');
+
+                            if (data?.name)
+                                this.updateStatusMessageOnUI(channel, data.name + " left the room")
+                            else
+                                this.updateStatusMessageOnUI(channel, "User " + data + " left the room")
+
+                            console.log(data, "left room");
+                        })
+                        .listenToAll((eventName, data) => {
+                            console.log("Event ::  "+ eventName + ", data is ::" + JSON.stringify(data));
+                        })
+                        .error((err)=> {
+                            console.error(err)
+                        })
+                });
+
+        },
 
         // Uses to send message to other clients using client-events.
         publishMessageOnPrivateChannel(event) {
